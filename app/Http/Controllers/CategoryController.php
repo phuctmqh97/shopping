f39747c4b944a444b9a75be7d62d15852a183364
Minh Phúc <67223957+phuctmqh97@users.noreply.github.com>
2021-10-10 02:13:13 +0700

    done

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -10,9 +11,44 @@ class CategoryController extends Controller
         return view('admin.category.index');
     }
 
+    private $htmlSelect;
+    public function __construct()
+    {
+        $this->htmlSlelect = '';
+    }
     public function addForm(){
-        return view('admin.category.add');
+       
+        // foreach ($data as $value) {
+        //     if ($value['parent_id'] == 0) {
+        //         echo " <option>". $value['name'] ."</option>";
+
+        //         foreach ($data as $value2) {
+        //             if ($value2['parent_id'] == $value['id']){
+        //             echo " <option>". $value2['name'] ."</option>";
+
+        //             foreach ($data as $value3) {
+        //                 if ($value3['parent_id'] == $value2['id']){
+        //                 echo " <option>". $value3['name'] ."</option>";
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        $htmlOption = $this->categoryRecusive(id:0);
+        return view('admin.category.add' ,compact('htmlOption'));
+        
     }
 
 
+    function categoryRecusive($id, $text = '')
+    {
+        $data = Category::all();
+        foreach ($data as $value) {
+            if ($value['parent_id'] == $id){
+                $this->htmlSelect .= " <option>". $text . $value['name'] ."</option>";
+                $this->categoryRecusive($value['id'], $text. '-');
+            }
+        }
+    return $this->htmlSelect;
+    }
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -42,6 +42,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
+                
             Route::prefix('admin')
                 ->middleware(['web'])
                 ->namespace($this->namespace)
